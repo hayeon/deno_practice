d9fd803843828d0fbed10e2ce1d14bdbe7a11434
양하연 <81798537+hayeon@users.noreply.github.com>
2021-07-04 13:43:50 +0900

    test

---
+++ createFile.ts
@@ -0,0 +1,7 @@
+const encoder = new TextEncoder();
+
+const helloText = encoder.encode("hello");
+
+ await Deno.writeFile("hello.txt", helloText);
+    
+
---
+++ dateTiem.ts
@@ -0,0 +1,5 @@
+import { dayOfYear,
+
+     } from "https://deno.land/std@0.100.0/datetime/mod.ts";
+
+console.log(dayOfYear(new Date("2021-07-04")));
